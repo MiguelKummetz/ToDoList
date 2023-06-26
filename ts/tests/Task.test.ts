b483a689910c639f5f9892eb215237651ae6f8ea
Ami <117275293+MiguelKummetz@users.noreply.github.com>
2023-06-26 11:48:08 +0200

    Add files via upload

--- ts/tests/Task.test.ts
+++ ts/tests/Task.test.ts
@@ -1,50 +1,41 @@
 import { Task } from "../src/Task";
 
-test(`given a name to a new Task we expect throw "toDTO" the correct created object`, () => {
-
-    // given o arrange
-    const name = 'test name'
-    const task = new Task(name)
-
-    // when o act
-    const taskDTO = task.toDTO()
-    
-    // then o assert
-    const taskExpected = { name, isDone: false, id:1}
-    expect(taskDTO).toEqual(taskExpected);
-});
-
-test(`given a new task when using "changeStatus" we expect isDone to be true`, () => {
-
-    const name = 'test name'
-    const task = new Task(name)
-
-    task.changeStatus()
-
-    const {isDone} = task.toDTO() // destructuring
-    expect(isDone).toBeTruthy
-});
-
-test(`given a name to a two new Task we expect throw "toDTO" see the correct ID assingment`, () => {
-
-    // given o arrange
+describe(`Testing 'Task' 1 -`, () => {
     const name = 'test name'
     const task1 = new Task(name)
     const task2 = new Task(name)
     const task3 = new Task(name)
-
-    // when o act
-    const taskDTO1 = task3.toDTO()
     
+    test(`1.given a name to a new Task we expect throw "toDTO" the correct created object`, () => {
+        
+        // given o arrange
     
-    // then o assert
-    const taskExpected = 5
-    expect(taskDTO1.id).toEqual(taskExpected);
-});
+        // when o act
+        const taskDTO = task1.toDTO()
+        
+        // then o assert
+        const taskExpected = { name: 'test name', isDone: false}
+        expect(taskDTO).toEqual(taskExpected);
+    });
 
+    test(`2.given a new task when using "changeStatus" we expect isDone to be true`, () => {
+    
+        task1.changeStatus()
+    
+        const {isDone} = task1.toDTO() // destructuring
+        expect(isDone).toBeTruthy
+    });
 
-// given o arrange
+    test(`3.given a name to a two new Task we expect throw "toDTO" see the correct ID assingment`, () => {
 
-// when o act
+        // given o arrange
+    
+        // when o act
+        const taskDTO = task3.toDTO()
+        
+        // then o assert
+        const taskExpected = 'test name'
+        expect(taskDTO.name).toEqual(taskExpected);
+    });
 
-// then o assert
+  });
--- ts/tests/TaskList.test.ts
+++ ts/tests/TaskList.test.ts
@@ -1,99 +1,82 @@
 import { Task } from "../src/Task";
 import { TaskList } from "../src/TaskList";
 
-test(`given a new task when using "add" then we expect the list with the object in it`, () => {
 
-    // given o arrange
-    const name = 'test name'
-    const task = new Task(name)
-    const list = new TaskList
-
-    // when o act
-    list.add(task)
-
-    // then o assert
-    const listExpected = {"list":[task]}
-    expect(list).toEqual(listExpected);
-});
-
-test(`given two new tasks when using "add" then we expect the list with the objects in it`, () => {
-
-    // given o arrange
-    const name = 'test name'
-    const task1 = new Task(name)
-    const task2 = new Task(name)
-    const list = new TaskList
-
-    // when o act
-    list.add(task1)
-    list.add(task2)
-
-    // then o assert
-    const listExpected = {"list":[task1,task2]}
-    expect(list).toEqual(listExpected);
-});
-
-test(`given a new tasks when using "delete" then we expect the list empty`, () => {
-
-    // given o arrange
-    const name = 'test name'
-    const task = new Task(name)
-    const list = new TaskList
-
-    // when o act
-    list.add(task)
-    list.delete(0)
-
-    // then o assert
-    const listExpected = {"list":[]}
-    expect(list).toEqual(listExpected);
-});
-
-test('given a list of tasks when using "find()" then we expect to get te correct task', () => {
-    //given
-    const list = new TaskList;
-    const task1 = new Task('Task')
-    const task2 = new Task('Task')
+describe(`Testing 'TaskList' 2 -`, () => {
+    const task1 = new Task('test name')
+    const task2 = new Task('Task to delete')
     const task3 = new Task('Task to be find')
-
-    //when
-    list.add(task1)
-    list.add(task2)
-    list.add(task3)
     
-    //then
-    const findedTask = list.find(3)
-    const taskExpected = {"id": 3, "isDone": false, "name": "Task to be find"}
+    test(`1.given a new task when using "add" then we expect the list with the object in it`, () => {    
+        const list1 = new TaskList;
+
+        list1.addTask(task1)
+
+        const listExpected = {'list':[task1]}
+        expect(list1).toEqual(listExpected);
+    });
+
+    test(`2.given two new tasks when using "add" then we expect the list with the objects in it`, () => {
+        const list1 = new TaskList;
+
+        list1.addTask(task2)
+
+        const listExpected = {'list':[task2]}
+        expect(list1).toEqual(listExpected);
+    });
+
+    test(`3.given a new tasks when using "delete" then we expect the list empty`, () => {
+        const list1 = new TaskList;
+
+        list1.addTask(task1)
+        list1.addTask(task2)
+        list1.deleteTask(0)
     
-    expect(findedTask).toEqual(taskExpected)
-})
+        const listExpected = {"list":[task2]}
+        expect(list1).toEqual(listExpected);
+    });
+    
+    test('4.given a list of tasks when using "find()" then we expect to get te correct task', () => {
+        const list1 = new TaskList;
+    
+        list1.addTask(task1)
+        list1.addTask(task2)
+        list1.addTask(task3)
+        
+        const findedTask = list1.findTask(2)
+        const taskExpected = {"isDone": false, "name": "Task to be find"}
+        
+        expect(findedTask).toEqual(taskExpected)
+    })
 
-test(`given a new tasks when using "delete" then we expect the first place of the array to be empty`, () => {
+    test(`5.given a new tasks when using "delete" then we expect the first place of the array to be empty`, () => {
+        const list1 = new TaskList
+    
+        list1.addTask(task1)
+        list1.addTask(task2)
+        list1.addTask(task3)
+        list1.deleteTask(1)
+    
+        const result = list1
+        const listExpected = {"list":[task1, task3]}
 
-    // given o arrange
-    const taskToDelete = new Task('taskToDelete')
-    const taskToSurvive = new Task('taskToSurvive')
-    const list = new TaskList
+        expect(result).toEqual(listExpected);
+    });
 
-    // when o act
-    list.add(taskToDelete)
-    list.add(taskToSurvive)
-    list.delete(0)
+    test(`6.given a list with tasks using "mapList" then we expect the list to be showed in console`, () => {
+        const list1 = new TaskList
+    
+        list1.addTask(task1)
+        list1.addTask(task2)
+        list1.addTask(task3)
 
-    // then o assert
-    const result = list /////////////////////////////////////////////////////////////////////////////
-    const Expected = list
-    expect(result).toEqual(Expected);
+        task2.changeStatus()
+
+        const expectedList = 
+        `1.test name: ⭕\n2.Task to delete: ✅\n3.Task to be find: ⭕\n`
+
+        const result = list1.mapList()
+    
+        expect(result).toEqual(expectedList)
+    })
 });
-
-test('given a new task when checking the ID then we expect to be 1', () => {
-    const task = new Task('test')
-    const taskList = new TaskList
-
-    taskList.add(task)
-
-    const expectedId = 1
-
-    expect(taskList.list[0]).toEqual(expectedId)
-    
-})
